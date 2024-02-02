49c90bbd977d07cc3aed3132d23860550e77865c
gideonn12 <gideoneeman@icloud.com>
2024-02-02 12:52:11 +0200

    changed appearance

--- src/SubscribePage/SubscribePage.js
+++ src/SubscribePage/SubscribePage.js
@@ -8,14 +8,10 @@ function SubscribePage(){
             <div className="foobar">
                 foobar
             </div>
-            <div className="foobartext">
-                Foobar helps you connect and share <br>
-                </br>with the people in your life.
-            </div>
         </div>
         <div className="subscribecontainer">
         <div className="sign">
-                Sign Up
+                Create a new account
             </div>
         <div className="signtext">
                 It's quick and easy.
@@ -30,7 +26,7 @@ function SubscribePage(){
                 <input type="password_check" placeholder="Enter Password again"/>
                 <br></br>
                 <button className="subscribe_btn">
-                    <h2>Create new account</h2>
+                    <h2>Sign Up</h2>
                 </button>
 
             </div>
