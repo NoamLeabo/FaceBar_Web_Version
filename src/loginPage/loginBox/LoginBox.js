11601c40ba2c71d5979908153a7124bc33e0805b
Arnon Lutsky <realsplashingfish@gmail.com>
2024-02-17 23:16:42 +0200

    - minor changes

--- src/loginPage/loginBox/LoginBox.js
+++ src/loginPage/loginBox/LoginBox.js
@@ -1,32 +1,31 @@
 import IBStyle from "./LoginBox.css";
 import InputGetter from "../../CrossScreensElements/inputGetter/InputGetter";
 import Btn from "../../CrossScreensElements/btn/Btn";
-import { useEffect, useState, useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import ForgotPasswordToast from "../toasts/ForgotPasswordToast";
 // import Toast from 'react-bootstrap/Toast';
 
-
-function LoginBox({activeUsers, SetLoggedUser}) {
-  const toastTrigger = useRef(null)
+function LoginBox({ activeUsers, SetLoggedUser }) {
+  const toastTrigger = useRef(null);
 
   const navigate = useNavigate();
   const [running, setRunning] = useState(false);
 
   const [entry, setEntry] = useState({ user: "", password: "" }); // Define entry state
-  const uName = useRef('');
-  const uPassword = useRef('');
+  const uName = useRef("");
+  const uPassword = useRef("");
 
-  const setUsername = function(newUsername) {
+  const setUsername = function (newUsername) {
     uName.current = newUsername;
-  }
-  const setPassword = function(newPassword) {
+  };
+  const setPassword = function (newPassword) {
     uPassword.current = newPassword;
-  }
-  
+  };
+
   useEffect(() => {
     if (running) {
-    checkIfValid(entry);
+      checkIfValid(entry);
     }
   }, [entry]);
 
@@ -45,41 +44,66 @@ function LoginBox({activeUsers, SetLoggedUser}) {
       }
     }
     if (!isValid) {
-      alert("Username or Password is incorrect!")
+      alert("Username or Password is incorrect!");
     }
-    if (isValid){
-      navigate("/home")
+    if (isValid) {
+      navigate("/home");
     }
   };
 
-  const newNavigation = function() {
-    console.log(555)
-    navigate("/details")
-  }
-    return(
-        <div className="col-xl-4 col-lg-4 col-md-5">
-          {/* Creating a card in which we will have all components for the login-Box */}
-            <div className="card loginCard">
-              {/* Creating the card-body */}
-              <div className="card-body">
-                {/* Putting the 'Username' input-Box */}
-                <InputGetter type="text" text="Username or Email" onChange={setUsername} />
-                {/* Putting the 'Password' input-Box */}
-                <InputGetter type="password" text="Password" onChange={setPassword} />
-                {/* Creating a Log-in btn */}
-                <Btn clicked={clickedLogIn} className={"fw-bolder btn"} id={"logInBtn"} text={"Log In"}/>
-                {/* Adding a 'forgot password?' linker */}
-                <a href="#" onClick={() => {navigate("/details"); alert("Create another account then :)")}} className="" style={{marginTop: '10px' }}>Forgot password?</a>
-                {/* Adding a visual divider */}
-                <hr style={{ margin: '10px 0' }} />
-                {/* Adding a 'create a new account' btn */}
-                <Btn clicked={newNavigation} className={"fw-bolder btn"} id={"cNewBtn"} text={"Create new account"}/>
-              </div>
-            </div>
-          {/* Adding some motivating text */}
-          <div id="genericT">Create a Page for a celebrity, brand, or business.</div>
+  const newNavigation = function () {
+    navigate("/details");
+  };
+  return (
+    <div className="col-xl-4 col-lg-4 col-md-5">
+      {/* Creating a card in which we will have all components for the login-Box */}
+      <div className="card loginCard">
+        {/* Creating the card-body */}
+        <div className="card-body">
+          {/* Putting the 'Username' input-Box */}
+          <InputGetter
+            type="text"
+            text="Username or Email"
+            onChange={setUsername}
+          />
+          {/* Putting the 'Password' input-Box */}
+          <InputGetter type="password" text="Password" onChange={setPassword} />
+          {/* Creating a Log-in btn */}
+          <Btn
+            clicked={clickedLogIn}
+            className={"fw-bolder btn"}
+            id={"logInBtn"}
+            text={"Log In"}
+          />
+          {/* Adding a 'forgot password?' linker */}
+          <a
+            href="#"
+            onClick={() => {
+              navigate("/details");
+              alert("Create another account then :)");
+            }}
+            className=""
+            style={{ marginTop: "10px" }}
+          >
+            Forgot password?
+          </a>
+          {/* Adding a visual divider */}
+          <hr style={{ margin: "10px 0" }} />
+          {/* Adding a 'create a new account' btn */}
+          <Btn
+            clicked={newNavigation}
+            className={"fw-bolder btn"}
+            id={"cNewBtn"}
+            text={"Create new account"}
+          />
         </div>
-    );
+      </div>
+      {/* Adding some motivating text */}
+      <div id="genericT">
+        Create a Page for a celebrity, brand, or business.
+      </div>
+    </div>
+  );
 }
 
 export default LoginBox;
