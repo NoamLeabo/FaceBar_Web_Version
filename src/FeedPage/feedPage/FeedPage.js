ac850e6a82ce41d8c8a4fc5ac136fa3448721ff2
Arnon Lutsky <realsplashingfish@gmail.com>
2024-03-13 23:23:37 +0200

    - For Hemi

--- src/FeedPage/feedPage/FeedPage.js
+++ src/FeedPage/feedPage/FeedPage.js
@@ -11,6 +11,7 @@ import { Navigate } from "react-router-dom";
 import Contact from "../contact/Contact";
 import { keyboard } from "@testing-library/user-event/dist/keyboard";
 import CreatePostButton from "../createPostButton/CreatePostButton";
+import LoadingPostPretty from "../loadingPostPretty/LoadingPostPretty";
 
 function FeedPage({
   loggedinUser,
@@ -246,82 +247,13 @@ function FeedPage({
                 {" "}
                 There are no posts to display...
               </h6>
-              <div className="card" aria-hidden="true">
-                {/* <img src="..." className="card-img-top" alt="..."></img> */}
-                <div className="card-body">
-                  <h5 className="card-title placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </h5>
-                  <p className="card-text placeholder-glow">
-                    <span className="placeholder col-7"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-6"></span>
-                    <span className="placeholder col-8"></span>
-                  </p>
-                  <a
-                    className="btn btn-primary disabled placeholder col-6"
-                    aria-disabled="true"
-                  ></a>
-                </div>
-              </div>
-              <div className="card" aria-hidden="true">
-                {/* <img src="..." className="card-img-top" alt="..."></img> */}
-                <div className="card-body">
-                  <h5 className="card-title placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </h5>
-                  <p className="card-text placeholder-glow">
-                    <span className="placeholder col-7"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-6"></span>
-                    <span className="placeholder col-8"></span>
-                  </p>
-                  <a
-                    className="btn btn-primary disabled placeholder col-6"
-                    aria-disabled="true"
-                  ></a>
-                </div>
-              </div>
-              <div className="card" aria-hidden="true">
-                {/* <img src="..." className="card-img-top" alt="..."></img> */}
-                <div className="card-body">
-                  <h5 className="card-title placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </h5>
-                  <p className="card-text placeholder-glow">
-                    <span className="placeholder col-7"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-6"></span>
-                    <span className="placeholder col-8"></span>
-                  </p>
-                  <a
-                    className="btn btn-primary disabled placeholder col-6"
-                    aria-disabled="true"
-                  ></a>
-                </div>
-              </div>
-              <div className="card" aria-hidden="true">
-                {/* <img src="..." className="card-img-top" alt="..."></img> */}
-                <div className="card-body">
-                  <h5 className="card-title placeholder-glow">
-                    <span className="placeholder col-6"></span>
-                  </h5>
-                  <p className="card-text placeholder-glow">
-                    <span className="placeholder col-7"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-4"></span>
-                    <span className="placeholder col-6"></span>
-                    <span className="placeholder col-8"></span>
-                  </p>
-                  <a
-                    className="btn btn-primary disabled placeholder col-6"
-                    aria-disabled="true"
-                  ></a>
-                </div>
-              </div>
+              <LoadingPostPretty />
+
+              <LoadingPostPretty />
+
+              <LoadingPostPretty />
+
+              <LoadingPostPretty />
             </div>
           )}
           <ContentNotAvailable />
